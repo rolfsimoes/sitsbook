c53110db06d1ac1e579da517bbea1677b89f0366
Felipe Carvalho <lipecaso@gmail.com>
2021-03-30 01:47:07 +0000

    add initial structure

---
+++ common.R
@@ -0,0 +1,21 @@
+set.seed(123)
+
+knitr::opts_chunk$set(
+    comment = "#>",
+    echo = TRUE,
+    cache = TRUE,
+    fig.retina = 0.8, # figures are either vectors or 300 dpi diagrams
+    dpi = 300,
+    out.width = "70%",
+    fig.width = 6,
+    fig.align = 'center',
+    fig.asp = 0.618,  # 1 / phi
+    fig.show = "hold"
+)
+
+# load essentials packages
+library(sits)
+library(sitsdata)
+library(sitsdocs)
+library(dtwclust)
+library(magrittr)
